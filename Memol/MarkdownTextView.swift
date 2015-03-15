92e7681174f7d515b8685a51ecea285e929b2d23
rakuishi <rakuishi@gmail.com>
2015-03-15 16:41:42 +0900

    Add MarkdownTextView

---
+++ Memol/MarkdownTextView.swift
@@ -0,0 +1,20 @@
+//
+//  MarkdownTextView.swift
+//  Memol
+//
+//  Created by OCHIISHI Koichiro on 2015/03/15.
+//  Copyright (c) 2015年 OCHIISHI Koichiro. All rights reserved.
+//
+
+import UIKit
+
+class MarkdownTextView: UITextView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
